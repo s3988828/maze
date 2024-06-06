74c4dd1d61dc405ea4e4a6b72302eb2924dfeed6
SEF-Assessment4 <sheen2906@gmail.com>
2024-06-06 14:17:23 +1000

    initial update

---
+++ mazeGenSkeleton2/generation/primGenerator.py
@@ -0,0 +1,27 @@
+# -------------------------------------------------------------------
+# DON'T CHANGE THIS FILE.
+# Prim's maze generator.
+#
+# __author__ = 'Jeffrey Chan'
+# __copyright__ = 'Copyright 2024, RMIT University'
+# -------------------------------------------------------------------
+
+
+from maze.maze3D import Maze3D
+from maze.util import Coordinates3D
+from generation.mazeGenerator import MazeGenerator
+
+
+
+class PrimMazeGenerator(MazeGenerator):
+    """
+    Prim's algorithm maze generator.  
+    TODO: Complete the implementation (Task A)
+    """
+	
+
+    def generateMaze(self, maze:Maze3D):
+        # TODO: Implement this method for task A.
+        pass
+
+        
